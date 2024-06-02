16956876077a4f1ac789b1415e6d1498c5274b9a
bachdx-1327 <duong.xuan.bach@sun-asterisk.com>
2024-06-02 19:06:23 +0700

    adding sth

--- src/utils/modules.py
+++ src/utils/modules.py
@@ -1,11 +1,31 @@
 from llama_index.core import Settings
-from llama_index.embeddings.azure_openai import AzureOpenAIEmbedding # type: ignore
+from llama_index.embeddings.azure_openai import AzureOpenAIEmbedding  # type: ignore
 from llama_index.llms.azure_openai import AzureOpenAI
 
 from .settings import Settings as AppSettings
 
 
 def setup_modules(settings: AppSettings):
+    """
+    Configure and set up the LLM and embedding models using Azure OpenAI settings.
+
+    This function initializes the language model (LLM) and embedding model using 
+    the Azure OpenAI configurations provided in the `settings`. The initialized models 
+    are then set as default in the `Settings` module for use throughout the application.
+
+    Args:
+        settings (AppSettings): The application settings containing Azure OpenAI configurations.
+            The settings should include:
+                - azure_openai.key: The API key for accessing the Azure OpenAI service.
+                - azure_openai.endpoint: The endpoint URL for the Azure OpenAI service.
+                - azure_openai.version: The API version of the Azure OpenAI service.
+                - azure_openai.gpt_deployment_name: The deployment name for the GPT model.
+                - azure_openai.embed_deployment_name: The deployment name for the embedding model.
+
+    Example:
+        settings = load_settings()
+        setup_modules(settings)
+    """
     api_key = settings.azure_openai.key
     end_point = settings.azure_openai.endpoint
     version = settings.azure_openai.version
--- src/utils/tracing.py
+++ src/utils/tracing.py
@@ -6,6 +6,25 @@ from .settings import Settings as AppSettings
 
 
 def setup_tracing(settings: AppSettings):
+    """
+    Configure and set up tracing for the application.
+
+    This function initializes the tracing callback handler using the tracing 
+    configurations provided in the `settings`. It then sets up the callback 
+    manager in the `Settings` module with the initialized tracing handler.
+
+    Args:
+        settings (AppSettings): The application settings containing tracing configurations.
+            The settings should include:
+                - tracing.public_key: The public key for the tracing service.
+                - tracing.secret_key: The secret key for the tracing service.
+                - tracing.host: The host URL for the tracing service.
+                - tracing.user_id: The user ID for the tracing service.
+
+    Example:
+        settings = load_settings()
+        setup_tracing(settings)
+    """
     callback_handler = LlamaIndexCallbackHandler(
         public_key=settings.tracing.public_key,
         secret_key=settings.tracing.secret_key,
