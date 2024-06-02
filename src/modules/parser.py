ae062a6b0295036827c11a1367b19a5a00565c6b
bachdx-1327 <duong.xuan.bach@sun-asterisk.com>
2024-06-02 07:31:38 +0700

    Commit to Adding Based code

---
+++ src/modules/parser.py
@@ -0,0 +1,62 @@
+from typing import List
+from pathlib import Path
+import warnings
+
+from llama_index.core import (
+    SimpleDirectoryReader,
+    Document)
+from llama_index.readers.file import (
+    DocxReader,
+    PDFReader)
+
+from llmsherpa.readers import LayoutPDFReader
+
+
+DEFAULT_LLMSHERPA_API_URL = "https://readers.llmsherpa.com/api/document/developer/parseDocument?renderFormat=all"
+
+
+class Parser:
+    def __init__(self,
+                 llmsherpa_api_url: str = DEFAULT_LLMSHERPA_API_URL):
+        self.pdf_reader = LayoutPDFReader(llmsherpa_api_url)
+        lmi_pdf_extractor = PDFReader()
+        lmi_docx_extractor = DocxReader()
+        self.lmi_file_extractor = {
+            ".pdf": lmi_pdf_extractor,
+            ".docx": lmi_docx_extractor
+        }
+
+    def parse_file(self, file_path: str,
+                   category: str = "Sun*") -> List[Document]:
+        try:
+            sherpa_documents = self.pdf_reader.read_pdf(file_path)
+            documents = []
+            # TODO: Convert Llmsherpa Document to Llama-index Document
+            if len(sherpa_documents.chunks()) > 0:
+                for i, chunk in enumerate(sherpa_documents.chunks()):
+                    file_name = Path(file_path).name
+                    text = chunk.to_context_text()
+                    id = f"{file_path}_{i}"
+                    documents.append(
+                        Document(
+                            text=text,
+                            id_=id,
+                            metadata={
+                                "name": file_name,
+                                "category": category
+                            },
+                            excluded_llm_metadata_keys=["file_path"]
+                        )
+                    )
+        except Exception as e:
+            warnings.warn(
+                f"Unexpected error {e} while reading {file_path},"
+                f"using default readers"
+            )
+            # TODO: Implement the metadata generation func
+            documents = SimpleDirectoryReader(
+                input_files=[file_path], file_extractor=self.lmi_file_extractor,
+                filename_as_id=True
+            ).load_data()
+
+        return documents
---
+++ src/utils/tracing.py
@@ -0,0 +1,15 @@
+from langfuse.llama_index import LlamaIndexCallbackHandler
+from llama_index.core.callbacks import CallbackManager
+from llama_index.core import Settings
+
+from .settings import Settings as AppSettings
+
+
+def setup_tracing(settings: AppSettings):
+    callback_handler = LlamaIndexCallbackHandler(
+        public_key=settings.tracing.public_key,
+        secret_key=settings.tracing.secret_key,
+        host=settings.tracing.host,
+        user_id=settings.tracing.user_id
+    )
+    Settings.callback_manager = CallbackManager([callback_handler])
